d68751e6811f4199eac2737100179ddcb0edcbe4
JustinCasler <97986810+JustinCasler@users.noreply.github.com>
2023-10-12 18:07:36 -0400

    Fix bug fixes and add password hashing (issue #3)

--- registration.php
+++ registration.php
@@ -1,36 +1,40 @@
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="application/x-www-form-urlencoded"/>
-<title>Sample Submission Form</title>
-<script>
-    function validateForm() {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
-        console.log(username)
-        if (username == null || username.length ==0){
-            alert("Username cannot be blank");
-            return false; // Prevent form submission
+    <head>
+    <meta http-equiv="Content-Type" content="Registration for PlaylistPulse"/>
+    <title>Registration Page</title>
+    <script>
+        function validateForm() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirmPassword").value;
+            console.log(username)
+            if (username == null || username.length ==0){
+                alert("Username cannot be blank");
+                // Prevent form submission
+                return false; 
+            }
+            if (password !== confirmPassword) {
+                alert("Passwords do not match!");
+                // Prevent form submission
+                return false; 
+            }
+            if (password.length < 10) {
+                alert("Password must be at least 10 characters long!");
+                // Prevent form submission
+                return false; 
+            }
+            // Allow form submission
+            return true; 
         }
-        if (password !== confirmPassword) {
-            alert("Passwords do not match!");
-            return false; // Prevent form submission
+        function clearForm() {
+            document.getElementById("username").value = "";
+            document.getElementById("password").value = "";
+            document.getElementById("confirmPassword").value = "";
+            // You can add more input fields to clear here
         }
-        if (password.length < 10) {
-            alert("Password must be at least 10 characters long!");
-            return false; // Prevent form submission
-        }
-        return true; // Allow form submission
-    }
-    function clearForm() {
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
-        document.getElementById("confirmPassword").value = "";
-        // You can add more input fields to clear here
-    }
-</script>
-</head>
+    </script>
+    </head>
 
 <!-- 
   HTML form displayed to the user
@@ -47,6 +51,8 @@
             <button type="submit" name="submit">Submit</button>
             <button type="button" name="clear" onclick="clearForm()">Clear</button>
         </form>
+        <p>
+        Already have an account? <a href="login.php">Login here</a>
     </body>
 </html>
 
@@ -54,22 +60,30 @@
     if (isset($_POST['submit'])) {
         register(); // Call the register function when the form is submitted
     }
-    function register() 
-    {
-        include_once 'includes/dbh.php' ;
+    function register() {
+        include_once 'includes/dbh.php';
+    
         $username = mysqli_real_escape_string($conn, $_POST['name']);
         $password = mysqli_real_escape_string($conn, $_POST['pwd']);
-
-        $sql_check = "SELECT * FROM users WHERE username = '$username';";
-        $result_check = mysqli_query($conn, $sql_check);
+    
+        $sql_check = "SELECT * FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql_check);
+        mysqli_stmt_bind_param($stmt, 's', $username);
+        mysqli_stmt_execute($stmt);
+        $result_check = mysqli_stmt_get_result($stmt);
+    
         if (mysqli_num_rows($result_check) > 0) {
             // Username already exists, so alert the user and do not insert into the database
             echo "Username already exists. Please choose another username.";
         } else {
-            // Username is unique, insert it into the database
-            $sql = "INSERT INTO users (username, pwd) VALUES ('$username', '$password');";
-            mysqli_query($conn, $sql);
-            echo "Registration successful!";
+            // Username is unique, hash the password and insert it into the database
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+            $sql = "INSERT INTO users (username, pwd) VALUES (?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, 'ss', $username, $hashed_password);
+            mysqli_stmt_execute($stmt);
+            //redirect to login
+            header('Location: login.php'); 
         }
     }
 ?>
